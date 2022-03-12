2cfa13f485c9f3030479094b10666c788a5f4813
luke <lukehoang195@gmail.com>
2022-03-12 17:06:17 +0700

    styling done

--- src/App.js
+++ src/App.js
@@ -37,15 +37,24 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <div className="button__wrapper">
-        <button onClick={stopTimer}>Stop</button>
-        <button ref={active} onClick={startTimer}>
-          Start
-        </button>
-        <button onClick={resetTimer}>Reset</button>
+    <div className="App container">
+      <h1>Coder Timer</h1>
+      <div className="timer__wrapper">
+        <div className="timer__display">
+          <p>{formatTime(time)}</p>
+        </div>
+        <div className="button__wrapper">
+          <button className="button" onClick={stopTimer}>
+            Stop
+          </button>
+          <button className="button" ref={active} onClick={startTimer}>
+            Start
+          </button>
+          <button className="button" onClick={resetTimer}>
+            Reset
+          </button>
+        </div>
       </div>
-      <div className="timer">Current : {formatTime(time)}</div>
     </div>
   );
 }
